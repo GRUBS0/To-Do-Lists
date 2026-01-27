f3bdefd63618a4ff464735c11db0faf973f86bff
Joseph <jjphillipse4@gmail.com>
2026-01-27 09:05:06 -0600

    Initial Commit

---
+++ To-Do Lists/To_Do_ListsApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_ListsApp.swift
+//  To-Do Lists
+//
+//  Created by joseph phillips on 1/27/26.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_ListsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
